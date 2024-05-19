9cbb2af875b6a146eaf17bde112aeb1e05988b78
Rohit Patwari <127968568+rp1425@users.noreply.github.com>
2024-05-19 16:56:08 +0530

    Update gaana.js

--- gaana.js
+++ gaana.js
@@ -26,7 +26,7 @@
 
 
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
